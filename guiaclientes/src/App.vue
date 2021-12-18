81afae3159c9a47af3a886b1ebaabc7d0c8057f4
MarcalFilosofo <58859672+MarcalFilosofo@users.noreply.github.com>
2021-12-18 11:20:34 -0300

    Exercicios

---
+++ guiaclientes/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>Guia Clientes</h1>
+    <div v-for="(cliente, index) in clientes" :key="cliente.id">
+      <h1>{{ index }}</h1>
+      <Cliente :cliente="cliente" />
+    </div>
+  </div>
+</template>
+
+<script> 
+import Cliente from './components/Cliente'
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      clientes: [
+        {
+          id: 1,
+          nome: 'João',
+          numero: '123456789',
+          email: 'joao@gmail.com'
+        }, {
+          id: 2,
+          nome: 'Maria',
+          numero: '987654321',
+          email: 'maria@gmail.com'
+        }, {
+          id: 3,
+          nome: 'Pedro',
+          numero: '123456789',
+          email: 'pedro@gmail.com'
+        }, {
+          id: 4,
+          nome: 'Ana',
+          numero: '987654321',
+          email: 'ana@gmail.com'
+        }, {
+          id: 5,
+          nome: 'José',
+          numero: '123456789',
+          email: 'josé@gmail.com'
+        }
+      ]
+    }
+  },
+  components: {
+    Cliente
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ guiaclientes/src/components/Cliente.vue
@@ -0,0 +1,49 @@
+<template>
+    <div :class="{'cliente' : !isPremimum, 'premium': isPremimum}">
+
+        <div>Nome: {{ nome }}</div>
+        <div>Número: {{ numero }}</div>
+        <div>Email: {{ email }}</div>
+        <div v-show="showIdade == true">Idade: {{ idade }}</div>
+        <!-- <div v-else>Não informada</div> -->
+        <!-- Segundos em um dia: {{ 60 * 60 * 24 }} -->
+        <button @click="mudarCor">Mudar Cor</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            isPremimum: true,
+        }
+    },
+    props: {
+        nome: String,
+        numero: String,
+        email: String,
+        idade: Number,
+        showIdade: Boolean
+    }, methods: {
+        mudarCor(){
+            this.isPremimum = !this.isPremimum;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .cliente {
+        font-size: 1.5em;
+        text-align: center;
+        color: #999;
+        margin-top: 50px;
+    }
+
+    .premium {
+        font-size: 1.5em;
+        text-align: center;
+        color: #ffc508;
+        margin-top: 50px;
+    }
+</style>
